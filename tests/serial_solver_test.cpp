4aec9b9dbf03fc26f87cc0897402d22f6af59c25
Martin Leung <martin.leung22@imperial.ac.uk>
2026-03-05 16:30:42 +0000

    turns out boost check close just wasn't the correct one for checking close to zero, I should have used boost check small

--- tests/serial_solver_test.cpp
+++ tests/serial_solver_test.cpp
@@ -1,9 +1,9 @@
 #include "serial_solver.h"
 
-#define BOOST_TEST_MODULE SerialSolver
+#define BOOST_TEST_MODULE SerialSolverTest
 #include <boost/test/included/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE ( isThisAName ){
+BOOST_AUTO_TEST_CASE ( ResidualTest ){
     SerialSolver ss(32, 32, 32, 1, 1e-7);
-    BOOST_CHECK_CLOSE ( ss.run_solver(), 1e-7, 0);
+    BOOST_CHECK_SMALL ( ss.run_solver(), 1e-7);
 }
